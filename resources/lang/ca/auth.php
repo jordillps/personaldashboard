d7618480559a4a6a3b928ada25041f52c8869b9b
jordillps <jordillps@gmail.com>
2020-05-14 10:34:15 +0200

    Commit translation

---
+++ resources/lang/ca/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Aquestes credencials no coincideixen amb els nostres registres.',
+    'throttle' => 'Massa intents fallits en molt poc temps. Si us plau intenti de nou a: seconds segons.',
+    'verify'=>'Verifiqui la seva adreça de correu electrònic',
+    'verification link' => 'Hem enviat al seu correu electrònic un enllaç de verificació',
+
+];
---
+++ resources/lang/ca/global.php
@@ -0,0 +1,134 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Global issues
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+
+    'login' => 'Accés',
+    'register' => 'Registrar-se',
+    'home' => 'Inici',
+    'reserve' => 'Reservar',
+    'dashboard' => 'Tauler de gestió personal',
+    'logged' => 'Sessió iniciada',
+    'logout' => 'Tancar sesió',
+    'back' => 'Tornar',
+    'Sign in with Google' => 'Accedir amb Google',
+    'Sign in with Facebook' => 'Accedir amb Facebook',
+    'Sign up with Google' => 'Registrar-se amb Google',
+    'Sign up with Facebook' => 'Registrarse amb Facebook',
+    'contact' => 'Contacte',
+    'agenda' => 'Agenda Personal',
+    'organize'=>'Organitza el teu dia a dia',
+    'profile'=>'El meu perfil',
+    'activity'=>'Activitats',
+    'welcome' =>'Benvingut   ',
+    'dashboard'=>'Tauler de resultats',
+    'pages'=>'Pàgines',
+    'charts'=>'Gràfics',
+    'tables'=>'Taules',
+    'users'=>'Usuaris',
+    'id'=>'Identificador',
+    'role'=>'Rol',
+    'name'=>'Nom',
+    'email'=>'Email',
+    'created'=>'Data d\'alta',
+    'image'=>'Imatge',
+    'action'=> 'Acció',
+    'No users' =>'No hi ha usuaris per a mostrar',
+    'select language'=>'Idioma',
+    'spanish' =>'Espanyol',
+    'english'=>'Anglès',
+    'search' => 'Cercar...',
+    'exportToExcel' => 'Exportar a Excel',
+
+    //Calendar
+    'calendar' =>'Calendari',
+    'eventadded'=>'Esdeveniment afegit correctament',
+    'titleevent' =>'Títol de l\'esdeveniment',
+    'locationevent'=>'Lloc de l\'esdeveniment',
+    'eventupdated'=>'Esdeveniment actualitzat',
+    'eventdeleteconfirm' => 'Estàs segur d\'esborrar l\'esdeveniment?',
+    'eventdeletedsuccessfully' => 'Esdeveniment esborrat correctament',
+    'eventdropinfo' =>'s\'ha modificat al ',
+    'eventdroppedconfirm' => 'Segur que vol modificar l\'esdeveniment?',
+    'createeventsinfo' => 'Clic per a crear un esdeveniment',
+    'updateeventsinfo' => 'Arrossegar i deixar anar per a modificar un esdeveniment',
+    'deleteeventsinfo' => 'Clic sobre un esdeveniment per a esborrar',
+
+    //Profile
+    'firstname' => 'Nom',
+    'lastname' => 'Cognoms',
+    'emailaddress'=>'Correu electrònic',
+    'telephone'=> 'Telèfon',
+    'birthdate'=> 'Data de naixement',
+    'password'=> 'Contrasenya',
+    'confirmpassword'=>'Confirma contrasenya',
+    'postalcode'=> 'Codi Postal',
+    'city'=>'Ciutat',
+    'update' =>'Actualitza Perfil',
+    'updatepassword'=>'Activar actualització password',
+    'imageprofile' => 'Imatge d\'usuari',
+    'imagevalidation' => 'Escull una imatge de perfil. max 2Mb, max alçada 300, max ample 300',
+    'userupdatedcorrectly'=> 'Usuari actualitzat correctament',
+    'googlecalendarid' => 'Google Calendar Id',
+    'googlecalendarapikey' => 'Google Calendar Api Key',
+
+    //Charts
+    'chart' => 'Gràfic',
+
+    //Tables
+    'deleteconfirmationtitle'=> 'Confirmació d\esborrat d\'usuaris',
+    'deleteconfirmationmessage' => 'Segur que vol esborrar l\'usuari?',
+    'deleteconfirmationcancel' => 'Cancel·lar',
+    'deleteconfirmationdelete' => 'Esborrar',
+    'userremovedcorrectly' => 'Usuari eliminat correctament',
+    'errordeletinguser' => 'Error en eliminar l\'usuari',
+
+    //Email verification
+    'emailverification' => 'Verificació email',
+    'checkyouremailbox' => 'Hem enviat al seu correu electrònic un enllaç de verificació',
+
+    //Email New user registered
+    'newuserregisterd' => 'Nou usuari registrat a Personal Dashboard',
+    'namenewuser' => 'Nom',
+    'emailnewuser' => 'Correu Electrònic',
+    'thanks' => 'Gràcies',
+
+    //Import
+    'import' => 'Importar dades',
+    'importFromExcel' => 'Importar dades des de Excel',
+    'dataimport' => 'Dades importades',
+    'nopartners' => 'Encara no hi ha socis registrats',
+    'partners' => 'Llistat de Socis',
+    'importedcorrectly' => 'Les dades s\'han importat correctament',
+    'printpdf' => 'Generar PDF',
+
+    //Reservations
+    'reservations' => 'Reserves',
+    'phone' => 'Telèfon',
+    'reservation_date' => 'Data',
+    'slot' => 'Hora',
+    'confirmdelete' => 'Confirmació de l\'esborrat',
+    'sure' => 'Estás segur?',
+    'cancel' => 'Cancel·lar',
+    'delete' => 'Esborrar',
+    'reservationdeleted' => 'Reserva esborrada correctament',
+    'reservationscalendar' => 'Calendari de reserves',
+    'namereservation' => 'Nom',
+    'emailreservation' => 'Correu electrònic',
+    'phonereservation' => 'Telèfon',
+    'createreservationsinfo' => 'Clic per a crear una reserva',
+    'updatereservationsinfo' => 'Arrossegar i deixar anar per a modificar una reserva',
+    'deletereservationsinfo' => 'Clic sobre un reserva per a esborrar',
+    'eventdroppednoslot' => 'La durada de la reserva no pot ser superior a 30 min',
+
+
+    //emails
+    'emailconfirmation' => 'Confirmació de reserva',
+];
